25b6804b93343c85cb12fc23eb70448fa6397abc
jhe04 <jielun.he@live.ca>
2022-05-11 16:04:25 -0400

    added eventlistener

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-//add a submit event listener on the form
-
-// stop the page from refreshing when the form is submitted
-
-// grab the user's to-do item from the form input
-
-// display to do on the page (within an li)
-// include a checkbox icon within the li
-
-// add li element to the ul
-
 // clicking on a task allows you to toggle between checked/unchecked
+
+const formElement = document.querySelector("form");
+console.log(formElement);
+
+//add a submit event listener on the form
+formElement.addEventListener("submit", function (e) {
+  // stop the page from refreshing when the form is submitted
+  e.preventDefault();
+
+  const inputElement = document.querySelector("#toDoItem");
+
+  // check that user has entered in a value
+  if (inputElement.value) {
+    // grab the user's to-do item from the form input and put into an li
+    const liElement = document.createElement("li");
+    liElement.innerHTML = `<i class="fa-regular fa-square"></i>`;
+    console.log(liElement);
+    // add li element to the ul
+    document.querySelector("main ul").appendChild(liElement);
+    // display to do on the page (within an li)
+    // include a checkbox icon within the li
+  } else {
+    console.log(`please enter in a value`);
+  }
+  // clear the input
+  inputElement.value = "";
+});
