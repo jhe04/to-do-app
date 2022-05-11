d00b010df34d575908ff49c474ea55ca7bad087d
jhe04 <jielun.he@live.ca>
2022-05-11 14:40:00 -0400

    added pseudo code

---
+++ script.js
@@ -0,0 +1,12 @@
+//add a submit event listener on the form
+
+// stop the page from refreshing when the form is submitted
+
+// grab the user's to-do item from the form input
+
+// display to do on the page (within an li)
+// include a checkbox icon within the li
+
+// add li element to the ul
+
+// clicking on a task allows you to toggle between checked/unchecked
